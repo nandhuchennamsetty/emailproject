c5f22c4b0b85a32e6b8eeee168330bde6ffd38ad
nandhu <nandhuchennamsettyl@gmail.com>
2025-12-27 11:49:13 +0530

    adding cors in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
 app.use(express.json());
 
 app.use('/api/templates', require('./routes/template.routes'));
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://emailproject-8zv0.onrender.com/api',
+  baseURL: 'https://emailproject-8zv0.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
