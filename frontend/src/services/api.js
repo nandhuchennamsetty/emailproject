2a50b45b7e111401998d248f40e4ad69ac8fb18b
nandhu <nandhuchennamsettyl@gmail.com>
2025-12-27 11:20:25 +0530

    change the api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://emailproject-8zv0.onrender.com/api',
+  baseURL: 'http://emailproject-8zv0.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
